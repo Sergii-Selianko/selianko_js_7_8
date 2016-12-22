e737c103aea354e9978dd1c07a42d9d48708cb0b
Sergii Selianko <sergii_selianko@ukr.net>
2016-12-22 23:21:04 +0200

    finish

--- js/script.js
+++ js/script.js
@@ -1,41 +1,41 @@
 // ********** Tab **********
 $(function(){
 
-    $(".link_tab").each(function(i){
-        $(this).attr("href", "#tab" + i);
+    $('.link_tab').each(function(i){
+        $(this).attr('href', '#tab' + i);
     });
 
-    $(".content").each(function(i){
-        $(this).attr("id", "tab" + i);
+    $('.content').each(function(i){
+        $(this).attr('id', 'tab' + i);
     });
 
-    $(".tab__container li").click(function(e){
+    $('.tab__container li').click(function(e){
         e.preventDefault();
-        $(".tab__container .active").removeClass("active");
-        $(this).addClass("active");
-        var tab = $(this).find("a").attr("href");
-        $(".content").not(tab).css({"display":"none"});
+        $('.tab__container .active').removeClass('active');
+        $(this).addClass('active');
+        var tab = $(this).find('a').attr('href');
+        $('.content').not(tab).css({'display':'none'});
         $(tab).fadeIn(200);
     });
 
 // ********** ToolTip **********
 
-    $("input").each(function() {
-        var tooltip = $(this).attr("title");
+    $('input').each(function() {
+        var tooltip = $(this).attr('title');
         var thisButton = $(this);
 
         $(this).mouseover(function() {
-            $(this).attr("title", "");
-            $("p#tooltip").remove();
+            $(this).attr('title', '');
+            $('p#tooltip').remove();
             $(this).parent().append('<p id="tooltip">' + tooltip + '</p>');
         });
 
         $(this).mouseout(function() {
-            $(this).attr("title", tooltip);
-            $("p#tooltip").remove();
+            $(this).attr('title', tooltip);
+            $('p#tooltip').remove();
         });
 
-        $(".button__form").click(function(e){
+        $('.button__form').click(function(e){
             e.preventDefault();
             $(thisButton).parent().append('<p id="tooltip">' + tooltip + '</p>');
         });
